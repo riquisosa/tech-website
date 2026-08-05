ff81aeb8af3493bf5c4fe55cc5fdbee97c997d44
riquisosa <riquiwritesthings@gmail.com>
2026-08-05 17:22:12 -0400

    Update en.ts

--- src/i18n/lang/en.ts
+++ src/i18n/lang/en.ts
@@ -56,7 +56,7 @@ export default {
 
     glossaryTitle: "Glossary",
     glossaryDesc: "Terms I've explained so you don't have to Google them.",
-    glossaryMentionedIn: "Mentioned in",
+    glossaryMentionedIn: "Mentions",
   },
   a11y: {
     skipToContent: "Skip to content",
